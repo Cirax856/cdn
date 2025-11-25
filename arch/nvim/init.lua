6c2483614dda4544050999c694e846abf194bc60
Cirax856 <Michal.Schneider07@seznam.cz>
2025-11-25 14:27:13 +0100

    added arch config files

---
+++ arch/nvim/init.lua
@@ -0,0 +1,19 @@
+require("config.lazy")
+require("config.remap")
+require("config.colors")
+require("lazy").setup("plugins")
+
+vim.opt.number = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.signcolumn = "no"
+vim.opt.fillchars = "eob: "
+
+vim.g.netrw_banner = 0
+vim.g.netrw_hide = 0
